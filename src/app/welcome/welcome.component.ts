8ddc7d63c740d7d0eeccbccb82c13ea33c9d9ef2
IamGiel <november14giel@gmail.com>
2019-04-14 21:14:22 -0400

    we added routing parameter using router.navigate add :name to route

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { routerNgProbeToken } from "@angular/router/src/router_module";
 
 @Component({
-  selector: 'app-welcome',
-  templateUrl: './welcome.component.html',
-  styleUrls: ['./welcome.component.css']
+  selector: "app-welcome",
+  templateUrl: "./welcome.component.html",
+  styleUrls: ["./welcome.component.css"]
 })
 export class WelcomeComponent implements OnInit {
-
-  constructor() { }
+  name;
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
+    this.name = this.route.snapshot.params["name"];
   }
-
 }
