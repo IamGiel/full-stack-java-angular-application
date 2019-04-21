83e76427d7bd9a700fc3e9e46f3e4443d3942e05
IamGiel <november14giel@gmail.com>
2019-04-21 18:29:26 -0400

    handle error

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -12,6 +12,8 @@ export class WelcomeComponent implements OnInit {
   name;
   customMessage;
   welcomeMessageReceived = false;
+  errorMessageReceived = false;
+  errorResponse;
   constructor(
     private welcomeService: WelcomeDataService,
     private activatedRoute: ActivatedRoute,
@@ -31,13 +33,26 @@ export class WelcomeComponent implements OnInit {
         this.welcomeMessageReceived = true;
         this.handleSuccessfulResponse(response.message);
         this.customMessage = response.message;
+      },
+
+      error => {
+        this.errorMessageReceived = true;
+        this.handleErrorResponse(error);
+        this.errorResponse = this.handleErrorResponse(error);
+        console.log("error response " + this.errorResponse);
       }
     );
+    this.handleErrorResponse(this.errorResponse);
     console.log("last line of getWelcomeMessage");
   }
 
   handleSuccessfulResponse(response) {
     console.log("handleSuccessfulResponse " + JSON.stringify(response));
+  }
+
+  handleErrorResponse(error) {
+    console.log("handleErrorResponse " + JSON.stringify(error.error.message));
+    return error.error.message;
   }
 
   goToDoListPage() {
